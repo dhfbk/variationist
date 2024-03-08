052008d12738892984beed39fe007bb63ee4624d
StefanoMenini <menini.stefano@gmail.com>
2024-03-08 11:06:46 +0100

    Update corpus_statistics.py

--- src/methods/corpus_statistics.py
+++ src/methods/corpus_statistics.py
@@ -1,6 +1,6 @@
 from statistics import stdev, mean
 
-def number_of_sentences(label_values_dict, subsets_of_interest):  
+def number_of_texts(label_values_dict, subsets_of_interest):  
     values_dict = dict()
     for column in label_values_dict:
         for l in range(len(label_values_dict[column])):
@@ -8,14 +8,14 @@ def number_of_sentences(label_values_dict, subsets_of_interest):
             values_dict[curr_label] = len(subsets_of_interest[column][l])
     return values_dict
 
-def average_sentence_lengt(label_values_dict, subsets_of_interest):  
+def average_text_lengt(label_values_dict, subsets_of_interest):  
     values_dict = dict()
     for column in label_values_dict:
         for l in range(len(label_values_dict[column])):
             values_list = []
             curr_label = subsets_of_interest[column][l].name
-            for sentence in subsets_of_interest[column][l]:
-                values_list.append(len(sentence))
+            for text in subsets_of_interest[column][l]:
+                values_list.append(len(text))
             values_dict[curr_label] = dict()
             values_dict[curr_label]["mean"] = mean(values_list)
             values_dict[curr_label]["stdev"] = stdev(values_list)
@@ -23,7 +23,7 @@ def average_sentence_lengt(label_values_dict, subsets_of_interest):
 
 def compute_basic_stats(label_values_dict, subsets_of_interest, args):  
     stats_dict = dict()
-    stats_dict["number_of_sentences"] = number_of_sentences(label_values_dict, subsets_of_interest)
-    stats_dict["average_sentence_length"] = average_sentence_lengt(label_values_dict, subsets_of_interest)
+    stats_dict["number_of_texts"] = number_of_texts(label_values_dict, subsets_of_interest)
+    stats_dict["average_text_length"] = average_text_lengt(label_values_dict, subsets_of_interest)
     print(stats_dict)
     return stats_dict
